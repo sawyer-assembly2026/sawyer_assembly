c2fffd4911c0dcd688ad1d3d1ff01b228a387b7c
sawyer-assembly2026 <hquijadasis@hotmail.com>
2026-03-09 17:15:47 -0600

    Update home sequence with images

--- sawyer_sequences/scripts/sawyer_home.py
+++ sawyer_sequences/scripts/sawyer_home.py
@@ -5,10 +5,16 @@ Created on Wed Feb  4 00:58:20 2026
 @author: hquij
 """
 
+import sys
 import rospy
+import rospkg
 import time
 from devices_interface import robot_ctl as robot
-
+from intera_interface import (
+    Navigator,
+    HeadDisplay,
+    Head
+)
 
 if __name__ == '__main__':
     
@@ -19,10 +25,31 @@ if __name__ == '__main__':
         #Create sawyer robot instance
         sawyer = robot.SawyerRobot()
 
+        #Create Navigator instance
+        navigator = Navigator()
+
+        #Create Head Display instance
+        headdisplay = HeadDisplay()
+        
+        #Create Head movements instance
+        head = Head()
+
+        #Get current package path
+        rospack = rospkg.RosPack()
+        package_path = rospack.get_path('sawyer_sequences')
+
+        #Display Starting home image
+        image_path = package_path + "/share/home_01.png"
+        headdisplay.display_image(image_path)
+
         sawyer.move_to_cartesian_relative(position=[0.0,0.0,0.2], orientation=[0.0,0.0,0.0], move_confirm=True, verbose=True)
 
         #Wait 1 seconds
         time.sleep(1.0)
+
+        #Display moving arm to home image
+        image_path = package_path + "/share/home_02.png"
+        headdisplay.display_image(image_path)
 
         #Move robot to home position
         sawyer.move_to_home()
@@ -30,12 +57,29 @@ if __name__ == '__main__':
     	#Wait 2 seconds
         time.sleep(2.0)
 
+        #Display moving head pan to home image
+        image_path = package_path + "/share/home_03.png"
+        headdisplay.display_image(image_path)
+        head.set_pan(-0.5, speed=0.3)
+
         #Green signalizes idle state
-        if sawyer._is_clicksmart == True:
-                sawyer.set_green_light()
+        #if sawyer._is_clicksmart == True:
+        #        sawyer.set_green_light()
         
+        #Display Homing finished image
+        image_path = package_path + "/share/home_04.png"
+        headdisplay.display_image(image_path)
+
+        #Wait 2 seconds
+        time.sleep(2.0)
+
+        #Display Intera SDK home image
+        #image_path = package_path + "/share/sawyer_sdk_research.png"
+        #headdisplay.display_image(image_path)
+
         # Explicitly shutdown the node after the duration
         rospy.signal_shutdown("Finishing testing node")
+        sys.exit()
     
     except rospy.ROSInterruptException:
         
